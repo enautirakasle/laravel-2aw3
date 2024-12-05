fc2bfdbca11dab333259338fcb1e0d57ef326251
Eñaut Agirre Arrizabalaga <enaut.irakasle@gmail.com>
2024-12-05 17:27:17 +0000

    controller eta noten probak

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return "view('welcome')";
 });
+
+Route::get('/note', [NoteController::class, 'index']);
